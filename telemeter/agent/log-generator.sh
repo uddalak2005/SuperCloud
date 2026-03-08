bb97d67ff21e6a8194736b49a98b31179e342531
Uddalak Mukhopadhyay <uddalakmukhopadhyay@gmail.com>
2026-03-08 23:01:35 +0530

    Gen Logs

--- telemeter/agent/log-generator.sh
+++ telemeter/agent/log-generator.sh
@@ -4,25 +4,27 @@ NODE_LOG="/var/log/node/app.log"
 REDIS_LOG="/var/log/redis/redis.log"
 MYSQL_LOG="/var/log/mysql/mysql.log"
 
-INTERVAL=1   # seconds between log entries
+INTERVAL=1
+
+# Ensure directories exist
+mkdir -p /var/log/node
+mkdir -p /var/log/redis
+mkdir -p /var/log/mysql
+
+touch $NODE_LOG $REDIS_LOG $MYSQL_LOG
 
 while true; do
     TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%S+00:00")
     PID=$((RANDOM % 9000 + 1000))
     RAND=$((RANDOM % 100))
 
-    # 80% INFO
     if [ $RAND -lt 80 ]; then
         echo "$TIMESTAMP INFO pid=$PID request processed successfully" >> $NODE_LOG
-    fi
 
-    # 15% WARN
-    if [ $RAND -ge 80 ] && [ $RAND -lt 95 ]; then
+    elif [ $RAND -lt 95 ]; then
         echo "$TIMESTAMP WARN cache miss for key=user_$PID" >> $REDIS_LOG
-    fi
 
-    # 5% ERROR
-    if [ $RAND -ge 95 ]; then
+    else
         echo "$TIMESTAMP ERROR pid=$PID query failed due to timeout" >> $MYSQL_LOG
     fi
 
--- telemeter/entrypoint.sh
+++ telemeter/entrypoint.sh
@@ -3,10 +3,10 @@
 set -e
 
 #To be removed later
-make -C agent setup-test-logs &
-sleep 1
-make -C agent write-test-logs &
-sleep 1
+# make -C agent setup-test-logs &
+# sleep 1
+# make -C agent write-test-logs &
+# sleep 1
 make -C agent gen-logs &
 
 
